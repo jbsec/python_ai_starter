3321bb78d2d71a0e74c99ace58f9dfac1d3c7cdb
James Barrett <45885505+JNBarr@users.noreply.github.com>
2020-06-18 22:55:01 +0100

    Add files via upload

---
+++ facerec_ai.py
@@ -0,0 +1,69 @@
+# original idea credit to ai hub
+# a NUMBER of addtional modules and 
+# libaries are required:
+# python 2.7 - 3. opencv3/4 numpy, HCFC
+"""
+import cv2
+import sys
+faceCascade = cv2.CascadeClassifer("haarcascade_frontalface_default.xml")
+video_capture = cv2.video_capture(0)
+
+##### algorithim #####
+while True:
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize(30, 30)
+        #flags=cv2.CV_HAAR_SCALE_IMAGE
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        breakvideo_capture.release()
+    cv2.destroyAllWindows()
+""" # delete comments to run 
+    # read function reads frames from source
+    # each loop
+# original script...
+import cv2 # requires distrubtion install (external)
+import sys
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+        #flags=cv2.CV_HAAR_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
